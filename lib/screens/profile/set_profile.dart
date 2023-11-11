a4b17a12a41ad9891e0e8c532b291f7ea2007b31
omeralvin <102193167+omeralvin@users.noreply.github.com>
2023-11-11 14:11:26 +0700

    update bagian atas

--- lib/screens/profile/set_profile.dart
+++ lib/screens/profile/set_profile.dart
@@ -1,4 +1,7 @@
+import 'package:capstone_wms/classes/colors_collection.dart';
 import 'package:flutter/material.dart';
+
+import '../../classes/icons_collection.dart';
 
 class SetProfileScreen extends StatefulWidget {
   const SetProfileScreen({super.key});
@@ -8,20 +11,72 @@ class SetProfileScreen extends StatefulWidget {
 }
 
 class _SetProfileScreenState extends State<SetProfileScreen> {
+  ColorApp colorApp = ColorApp();
+  IconsCollection icons = IconsCollection();
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height * .15,
-            width: double.infinity,
-            // color: ,
-            child: const Stack(
-              children: [],
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: BoxDecoration(
+                color: colorApp.dark4,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15.0,
+                  vertical: 15.0,
+                ),
+                child: Stack(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 50,
+                          child: Row(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.arrow_back,
+                                  color: colorApp.secondaryColor,
+                                ),
+                              ),
+                              const SizedBox(width: 20),
+                              const Text(
+                                "Settings",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 50),
+                        const CircleAvatar(
+                          radius: 70.0,
+                          backgroundImage: NetworkImage(
+                              'https://picsum.photos/200/300?random=1'),
+                          backgroundColor: Colors.transparent,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
