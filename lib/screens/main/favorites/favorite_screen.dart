7af0997d9a22f50357110758ae3ed5fc85ad6174
michaelis2101 <hrfahrizzal@gmail.com>
2023-11-20 15:21:05 +0700

    fix: image url

--- lib/screens/main/favorites/favorite_screen.dart
+++ lib/screens/main/favorites/favorite_screen.dart
@@ -16,7 +16,7 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
   final List<Map<String, dynamic>> items = [
     {
       'imageURL':
-          'https://s3-alpha-sig.figma.com/img/4dd7/cdef/186384b0004738973918693df2770523?Expires=1700438400&Signature=QeRDVkX2rVOV1cWb91H7TAYsJI4II1KQiodWcB6D8OVtSYhPlPa~AvbWV06wqdfgtynOQaddBdQRAUK6gPzAL0uEJUv05gMf7etv0SjsBywbTP4j97EcEY7HGGyQvN6zLMvnn5U6zX7qzbdit1vPjDtQLA0Ir-J7RrXLWJXXmQokOMsHLOKcgkyqq-WvHJCsrX4D3FMa9SmrK4WaRIAkXfMS~xl2pUOM6YQgS3C9izkuCLEHlmBW~JyWLoABUOA8r5Lownu~BpBTnaU8O8F4Qrgwt5xiO-nx86ilXBG1Qr6jidZ4UKykczjWLt4Qt1W~jA~3HRtZGrrQ7Ia~qOY5dg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+          'https://images.unsplash.com/photo-1565610222536-ef125c59da2e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       'title': 'Gudang',
       'name': 'Warehouse Abadi',
       'location': 'Jakarta Barat',
@@ -26,7 +26,7 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
     },
     {
       'imageURL':
-          'https://s3-alpha-sig.figma.com/img/c6d4/c288/e966b58e23a25382ea166b2e1d0e99a9?Expires=1700438400&Signature=NP3W8opT96YveizTRhKsfNqrPEXe9vC8VSJUYQr0VKwHjCu-47aKMT41aeO5yOnkpCApc8rMXwXr8b~gak-uP8sRc9j6wj-b-d~Xtc3L1KX1cSwX3WZo1fdzGWGT1~7s4MPr4MEKZdFd7IT-im0Vg~MvZdL3VJeoYLjBGfWjKIH~MQB58oZVpLofh7~DFgs~0xMPqXkdcHflng3nMUKqON9f-Ad2vnp-Snp3djfnpHHIwwNq4V0eHTOKDfod98DDMpuaPkFtRyLK2IElzzrXDb6E0qgURtMcPblZoTiJ7d4Uw1RlX02dx7nvlIgz~24QEDsgwpKNYT5~PIDLZNJDvg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+          'https://images.unsplash.com/photo-1565610222536-ef125c59da2e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       'title': 'Gudang',
       'name': 'Adhi Jaya Gudang',
       'location': 'Purwokerto',
@@ -36,7 +36,7 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
     },
     {
       'imageURL':
-          'https://s3-alpha-sig.figma.com/img/28c4/107f/cafa129b2dd320b8ea90a11d26b2f656?Expires=1700438400&Signature=Qkc9cqPqV9XslSJ8M~gu8Zs-~rVZe9bxcsCr5Few55nVTDAOjKsscGLpm8QpdzuVarjQPyY4PBaNGPwl6YR5nhsU12nrMgCqcOaK747dxX12ioNMsmttj5BNUb59e9-ChUs6bTA8~wp5kn4yaIbeSGypXlYkMzR6ExzhvFPY26FcyYyXzZu0vtn9nsU~EG3LNROs5boezLqo5zxpi6m9D1-xpWh9cyyaTf4vPLs1UpRkpONZ5EGZ8omwc37IM-yLQNKxP93AD2ABFl16V2nM0qI2JcE0FPmZJIibc-hgAAq~5UAyWINOgacci5EZN7NLtlXsmOX3vOcahl1tLXEcVg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+          'https://images.unsplash.com/photo-1565610222536-ef125c59da2e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       'title': 'Gudang',
       'name': 'Adhimetal group',
       'location': 'Jakarta Selatan',
@@ -46,7 +46,7 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
     },
     {
       'imageURL':
-          'https://s3-alpha-sig.figma.com/img/a222/166d/a4e437c7b4cefeff125e392fc21a727c?Expires=1700438400&Signature=gEREaJyKBpAhm-0s6ssz7-AxGLMQcnY9FXVmPiLG20vDGDnwNem~mBQmgUITYc1VAPxDSBSLiAqrk~aMBb78HCsZgAnKxGef9deTCOtHo76ekwWm7q940z09aZRNZ5XXV~puc9~GIEJQ1ysaF8MIZpVbGQGfGkbbDaJh3TvnWy2GQXQPxZDrbYimCntfhJ4ojMJgyfYyvXgma-oVQb34-GPP7ypE3~z6gZB3ZHYIwNL9J9kgNpt6wQ2u~qwRdH0~sh2Rlm3fr3KvLdXrEHOtoghZlDQRJ11IhJPQxr309P6Z2D5L19uE9wiheMLoPE~WGK9GrnWg1hB9lbYw995JOA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+          'https://images.unsplash.com/photo-1565610222536-ef125c59da2e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       'title': 'Gudang',
       'name': 'Marga Jaya',
       'location': 'Purwokerto Barat',
