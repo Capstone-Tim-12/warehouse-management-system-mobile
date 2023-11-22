6c7e5ef3d02719e10d7bc318feb3e723ae38a330
itsMallix <amandamaulana00@gmail.com>
2023-11-22 22:41:00 +0700

    fix : fixing card home screen

--- lib/screens/main/dashboard/dashboard.dart
+++ lib/screens/main/dashboard/dashboard.dart
@@ -608,7 +608,6 @@ class RecommendedCardWidget extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       children: <Widget>[
         SizedBox(
-          height: 169,
           width: 180,
           child: Container(
             margin: const EdgeInsets.all(3.0),
@@ -670,12 +669,20 @@ class RecommendedCardWidget extends StatelessWidget {
         ),
         const SizedBox(width: 5),
         SizedBox(
-          height: 169,
           width: 180,
-          child: Card(
-            color: ColorApp().light4,
-            shape: RoundedRectangleBorder(
+          child: Container(
+            margin: const EdgeInsets.all(3.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
               borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: ColorApp().dark4,
+                  offset: const Offset(1, 1),
+                  spreadRadius: 1,
+                  blurRadius: 1.0,
+                )
+              ],
             ),
             clipBehavior: Clip.antiAliasWithSaveLayer,
             child: Column(
@@ -723,12 +730,20 @@ class RecommendedCardWidget extends StatelessWidget {
         ),
         const SizedBox(width: 5),
         SizedBox(
-          height: 169,
           width: 180,
-          child: Card(
-            color: ColorApp().light4,
-            shape: RoundedRectangleBorder(
+          child: Container(
+            margin: const EdgeInsets.all(3.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
               borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: ColorApp().dark4,
+                  offset: const Offset(1, 1),
+                  spreadRadius: 1,
+                  blurRadius: 1.0,
+                )
+              ],
             ),
             clipBehavior: Clip.antiAliasWithSaveLayer,
             child: Column(
@@ -776,12 +791,20 @@ class RecommendedCardWidget extends StatelessWidget {
         ),
         const SizedBox(width: 5),
         SizedBox(
-          height: 169,
           width: 180,
-          child: Card(
-            color: Colors.white,
-            shape: RoundedRectangleBorder(
+          child: Container(
+            margin: const EdgeInsets.all(3.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
               borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: ColorApp().dark4,
+                  offset: const Offset(1, 1),
+                  spreadRadius: 1,
+                  blurRadius: 1.0,
+                )
+              ],
             ),
             clipBehavior: Clip.antiAliasWithSaveLayer,
             child: Column(
@@ -829,12 +852,20 @@ class RecommendedCardWidget extends StatelessWidget {
         ),
         const SizedBox(width: 5),
         SizedBox(
-          height: 169,
           width: 180,
-          child: Card(
-            color: Colors.white,
-            shape: RoundedRectangleBorder(
+          child: Container(
+            margin: const EdgeInsets.all(3.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
               borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: ColorApp().dark4,
+                  offset: const Offset(1, 1),
+                  spreadRadius: 1,
+                  blurRadius: 1.0,
+                )
+              ],
             ),
             clipBehavior: Clip.antiAliasWithSaveLayer,
             child: Column(
@@ -882,65 +913,20 @@ class RecommendedCardWidget extends StatelessWidget {
         ),
         const SizedBox(width: 5),
         SizedBox(
-          height: 169,
           width: 180,
-          child: Card(
-            color: Colors.white,
-            shape: RoundedRectangleBorder(
+          child: Container(
+            margin: const EdgeInsets.all(3.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
               borderRadius: BorderRadius.circular(10),
-            ),
-            clipBehavior: Clip.antiAliasWithSaveLayer,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 130,
-                  child: Image.network(
-                    "https://images.unsplash.com/photo-1587293852726-70cdb56c2866?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d2FyZWhvdXNlfGVufDB8fDB8fHww",
-                    fit: BoxFit.fitHeight,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Warehouse Abadi",
-                        style: TextCollection.bodyNormal
-                            .copyWith(color: ColorApp().dark1, fontSize: 16),
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        "Jakarta Barat",
-                        style: TextCollection.bodySmall.copyWith(
-                          color: ColorApp().dark1,
-                          fontWeight: FontWeight.normal,
-                          fontSize: 12,
-                        ),
-                      ),
-                      const SizedBox(height: 15),
-                      Text(
-                        "RP.10 Jt/bln",
-                        style: TextCollection.bodyNormal.copyWith(
-                          color: ColorApp().secondaryColor,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+              boxShadow: [
+                BoxShadow(
+                  color: ColorApp().dark4,
+                  offset: const Offset(1, 1),
+                  spreadRadius: 1,
+                  blurRadius: 1.0,
+                )
               ],
-            ),
-          ),
-        ),
-        const SizedBox(width: 5),
-        SizedBox(
-          height: 169,
-          width: 180,
-          child: Card(
-            color: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
             ),
             clipBehavior: Clip.antiAliasWithSaveLayer,
             child: Column(
