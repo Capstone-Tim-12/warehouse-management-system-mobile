4e0eeb485e0f4d23fdd39fb21e71286a8d5bec49
itsMallix <amandamaulana00@gmail.com>
2023-11-22 22:15:08 +0700

    fix : card homescreen

--- lib/screens/main/dashboard/dashboard.dart
+++ lib/screens/main/dashboard/dashboard.dart
@@ -160,7 +160,7 @@ class _DashboardState extends State<Dashboard> {
             const Padding(
               padding: EdgeInsets.symmetric(horizontal: 16.0),
               child: SizedBox(
-                height: 244,
+                height: 240,
                 width: double.infinity,
                 child: RecommendedCardWidget(),
               ),
@@ -610,10 +610,19 @@ class RecommendedCardWidget extends StatelessWidget {
         SizedBox(
           height: 169,
           width: 180,
-          child: Card(
-            color: ColorApp().light4,
-            shape: RoundedRectangleBorder(
+          child: Container(
+            margin: const EdgeInsets.all(3.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
               borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: ColorApp().dark4,
+                  offset: const Offset(1, 1),
+                  spreadRadius: 1,
+                  blurRadius: 1.0,
+                )
+              ],
             ),
             clipBehavior: Clip.antiAliasWithSaveLayer,
             child: Column(
@@ -770,7 +779,7 @@ class RecommendedCardWidget extends StatelessWidget {
           height: 169,
           width: 180,
           child: Card(
-            color: ColorApp().light4,
+            color: Colors.white,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10),
             ),
@@ -823,7 +832,7 @@ class RecommendedCardWidget extends StatelessWidget {
           height: 169,
           width: 180,
           child: Card(
-            color: ColorApp().light4,
+            color: Colors.white,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10),
             ),
@@ -876,7 +885,60 @@ class RecommendedCardWidget extends StatelessWidget {
           height: 169,
           width: 180,
           child: Card(
-            color: ColorApp().light4,
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 130,
+                  child: Image.network(
+                    "https://images.unsplash.com/photo-1587293852726-70cdb56c2866?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d2FyZWhvdXNlfGVufDB8fDB8fHww",
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Warehouse Abadi",
+                        style: TextCollection.bodyNormal
+                            .copyWith(color: ColorApp().dark1, fontSize: 16),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        "Jakarta Barat",
+                        style: TextCollection.bodySmall.copyWith(
+                          color: ColorApp().dark1,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 12,
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      Text(
+                        "RP.10 Jt/bln",
+                        style: TextCollection.bodyNormal.copyWith(
+                          color: ColorApp().secondaryColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(width: 5),
+        SizedBox(
+          height: 169,
+          width: 180,
+          child: Card(
+            color: Colors.white,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10),
             ),
