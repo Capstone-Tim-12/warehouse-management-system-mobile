3b78c16931a722f8d473c06ae69f415984d46fde
itsMallix <amandamaulana00@gmail.com>
2023-12-01 16:21:49 +0700

    fix : main payment untfinisehd

--- lib/screens/payment/payment_screen_main.dart
+++ lib/screens/payment/payment_screen_main.dart
@@ -1,5 +1,4 @@
 import 'package:capstone_wms/classes/colors_collection.dart';
-import 'package:capstone_wms/classes/inputstyle_collection.dart';
 import 'package:capstone_wms/classes/text_collection.dart';
 import 'package:capstone_wms/screens/payment/payment_screen_failed.dart';
 import 'package:capstone_wms/screens/payment/payment_screen_success.dart';
@@ -14,6 +13,8 @@ class PaymentScreen extends StatefulWidget {
 }
 
 class _PaymentScreenState extends State<PaymentScreen> {
+  bool showCardTextField = false;
+  TextEditingController cardController = TextEditingController();
   String? selectedPaymentMethod;
   @override
   Widget build(BuildContext context) {
@@ -29,160 +30,248 @@ class _PaymentScreenState extends State<PaymentScreen> {
           style: TextCollection().heading6,
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Detail Pemesanan",
-              style: TextCollection().bodySmall,
-            ),
-            const SizedBox(height: 10),
-            Container(
-              height: 105,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                border: Border.all(color: ColorApp().dark4),
-                borderRadius: BorderRadius.circular(12),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Detail Pemesanan",
+                style: TextCollection().bodySmall,
               ),
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: 140,
-                    height: 105,
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.horizontal(
-                        left: Radius.circular(12),
-                      ),
-                      child: Image.network(
-                        "https://images.unsplash.com/photo-1590247813693-5541d1c609fd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8d2FyZWhvdXNlfGVufDB8fDB8fHww",
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Warehouse Kita",
-                        style: TextCollection().heading6,
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        "Jakarta Barat",
-                        style: TextCollection().bodySmall.copyWith(
-                              fontWeight: FontWeight.normal,
-                            ),
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        "20.000.000/bulan",
-                        style: TextCollection().bodySmall.copyWith(
-                              fontWeight: FontWeight.normal,
-                            ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-            const SizedBox(height: 30),
-            Text(
-              "Metode Pembayaran",
-              style: TextCollection().bodySmall,
-            ),
-            const SizedBox(height: 10),
-            const Divider(),
-            const SizedBox(height: 10),
-            SizedBox(
-              height: 70,
-              width: double.infinity,
-              child: Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Kartu Kredit/Debit",
-                        style: TextCollection().heading6,
-                      ),
-                      const SizedBox(height: 10),
-                      SizedBox(
-                        width: 120,
-                        height: 20,
-                        child: SvgPicture.asset("assets/svg/mastercard.svg"),
-                      )
-                    ],
-                  ),
-                  const Spacer(),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.keyboard_arrow_down_rounded,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const Divider(),
-            const SizedBox(height: 10),
-            SizedBox(
-              height: 70,
-              width: double.infinity,
-              child: Row(
-                children: [
-                  const SizedBox(height: 5),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "ATM/BANK transfer",
-                        style: TextCollection().heading6,
-                      ),
-                      SizedBox(
-                        width: 200,
-                        height: 35,
-                        child: SvgPicture.asset(
-                          "assets/svg/bank_logo.svg",
-                          fit: BoxFit.fill,
+              const SizedBox(height: 10),
+              Container(
+                height: 105,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(color: ColorApp().dark4),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 140,
+                      height: 105,
+                      child: ClipRRect(
+                        borderRadius: const BorderRadius.horizontal(
+                          left: Radius.circular(12),
                         ),
-                      )
-                    ],
-                  ),
-                  const Spacer(),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.keyboard_arrow_down_rounded,
+                        child: Image.network(
+                          "https://images.unsplash.com/photo-1590247813693-5541d1c609fd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8d2FyZWhvdXNlfGVufDB8fDB8fHww",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
-                  ),
-                ],
+                    const SizedBox(width: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Warehouse Kita",
+                          style: TextCollection().heading6,
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          "Jakarta Barat",
+                          style: TextCollection().bodySmall.copyWith(
+                                fontWeight: FontWeight.normal,
+                              ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          "20.000.000/bulan",
+                          style: TextCollection().bodySmall.copyWith(
+                                fontWeight: FontWeight.normal,
+                              ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-            const Divider(),
-            SizedBox(
-              height: 60,
-              width: double.infinity,
-              child: Row(
-                children: [
-                  Text(
-                    "Metode pembayaran lain",
-                    style: TextCollection().heading6,
-                  ),
-                  const Spacer(),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.keyboard_arrow_down_rounded,
+              const SizedBox(height: 30),
+              Text(
+                "Metode Pembayaran",
+                style: TextCollection().bodySmall,
+              ),
+              const SizedBox(height: 10),
+              const Divider(),
+              const SizedBox(height: 10),
+
+              // SizedBox(
+              //   height: 70,
+              //   width: double.infinity,
+              //   child: Column(
+              //     crossAxisAlignment: CrossAxisAlignment.start,
+              //     children: [
+              //       Text(
+              //         "Kartu Kredit/Debit",
+              //         style: TextCollection().heading6,
+              //       ),
+              //       const SizedBox(height: 10),
+              //       Row(
+              //         children: [
+              //           Text(
+              //             showCardTextField
+              //                 ? "Visa"
+              //                 : selectedPaymentMethod ?? "Pilih Kartu",
+              //             style: TextCollection().bodySmall,
+              //           ),
+              //           const Spacer(),
+              //           IconButton(
+              //             onPressed: () {
+              //               setState(() {
+              //                 showCardTextField = !showCardTextField;
+              //               });
+              //             },
+              //             icon: const Icon(
+              //               Icons.keyboard_arrow_down_rounded,
+              //             ),
+              //           ),
+              //         ],
+              //       ),
+              //       if (showCardTextField)
+              //         Container(
+              //           width:
+              //               200, // Set a specific width or use constraints as needed
+              //           child: TextField(
+              //             controller: cardController,
+              //             decoration: InputDecoration(
+              //               hintText: 'Masukkan nomor kartu',
+              //               labelText: 'Nomor Kartu',
+              //             ),
+              //           ),
+              //         ),
+              //     ],
+              //   ),
+              // ),
+              // SizedBox(
+              //   height: 70,
+              //   width: double.infinity,
+              //   child: Visibility(
+              //     visible:
+              //         !showCardTextField, // Hide when the card text field is visible
+              //     child: Row(
+              //       children: [
+              //         const SizedBox(height: 5),
+              //         Column(
+              //           crossAxisAlignment: CrossAxisAlignment.start,
+              //           children: [
+              //             Text(
+              //               "ATM/BANK transfer",
+              //               style: TextCollection().heading6,
+              //             ),
+              //             SizedBox(
+              //               width: 200,
+              //               height: 35,
+              //               child: SvgPicture.asset(
+              //                 "assets/svg/bank_logo.svg",
+              //                 fit: BoxFit.fill,
+              //               ),
+              //             )
+              //           ],
+              //         ),
+              //         const Spacer(),
+              //         IconButton(
+              //           onPressed: () {},
+              //           icon: const Icon(
+              //             Icons.keyboard_arrow_down_rounded,
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //   ),
+              // ),
+
+              SizedBox(
+                height: 70,
+                width: double.infinity,
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Kartu Kredit/Debit",
+                          style: TextCollection().heading6,
+                        ),
+                        const SizedBox(height: 10),
+                        SizedBox(
+                          width: 120,
+                          height: 20,
+                          child: SvgPicture.asset("assets/svg/mastercard.svg"),
+                        )
+                      ],
                     ),
-                  ),
-                ],
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.keyboard_arrow_down_rounded,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+              const Divider(),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: 70,
+                width: double.infinity,
+                child: Row(
+                  children: [
+                    const SizedBox(height: 5),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "ATM/BANK transfer",
+                          style: TextCollection().heading6,
+                        ),
+                        SizedBox(
+                          width: 200,
+                          height: 35,
+                          child: SvgPicture.asset(
+                            "assets/svg/bank_logo.svg",
+                            fit: BoxFit.fill,
+                          ),
+                        )
+                      ],
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.keyboard_arrow_down_rounded,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(),
+              SizedBox(
+                height: 60,
+                width: double.infinity,
+                child: Row(
+                  children: [
+                    Text(
+                      "Metode pembayaran lain",
+                      style: TextCollection().heading6,
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.keyboard_arrow_down_rounded,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: Container(
