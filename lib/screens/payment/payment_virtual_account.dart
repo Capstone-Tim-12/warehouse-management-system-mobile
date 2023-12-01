70eed9f5d689e338ee0098b457eefb5d4a473a64
itsMallix <amandamaulana00@gmail.com>
2023-12-01 21:53:20 +0700

    fix screen va

--- lib/screens/payment/payment_virtual_account.dart
+++ lib/screens/payment/payment_virtual_account.dart
@@ -11,7 +11,9 @@ class VirtualAccountScreen extends StatefulWidget {
 }
 
 class _VirtualAccountScreenState extends State<VirtualAccountScreen> {
-  bool _isShow = true;
+  bool _isShowmBanking = true;
+  bool _isShowiBanking = true;
+  bool _isVisible = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -106,62 +108,169 @@ class _VirtualAccountScreenState extends State<VirtualAccountScreen> {
                   IconButton(
                     onPressed: () {
                       setState(() {
-                        _isShow = !_isShow;
+                        _isShowmBanking = !_isShowmBanking;
+                        _isShowiBanking = !_isShowiBanking;
+                        _isVisible = !_isVisible;
                       });
                     },
-                    icon: _isShow
-                        ? const Icon(Icons.keyboard_arrow_up)
-                        : const Icon(Icons.keyboard_arrow_down),
+                    icon: _isShowmBanking
+                        ? const Icon(Icons.keyboard_arrow_down)
+                        : const Icon(Icons.keyboard_arrow_up),
                   ),
                 ],
               ),
               const Divider(),
               const SizedBox(height: 10),
-              Visibility(
-                visible: _isShow,
-                maintainSize: true,
-                maintainAnimation: true,
-                maintainState: true,
-                child: SizedBox(
-                  width: double.infinity,
-                  child: RichText(
-                    text: TextSpan(
-                      text: "1. Pilih m-Transfer > BCA Virtual Account\n",
-                      style: TextCollection().bodySmall.copyWith(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 12,
-                            height: 2.0,
-                          ),
-                      children: <TextSpan>[
-                        const TextSpan(
-                          text: "2. Masukkan nomor Virtual Account ",
-                        ),
-                        TextSpan(
-                          text: " 123 23456 0000 222\n",
-                          style: TextCollection().smallLabel.copyWith(
-                                color: ColorApp().secondaryColor,
-                                fontWeight: FontWeight.bold,
+              Stack(
+                children: [
+                  Visibility(
+                    visible: !_isShowmBanking,
+                    maintainSize: true,
+                    maintainAnimation: true,
+                    maintainState: true,
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: RichText(
+                        text: TextSpan(
+                          text: "1. Pilih m-Transfer > BCA Virtual Account\n",
+                          style: TextCollection().bodySmall.copyWith(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 12,
+                                height: 2.0,
                               ),
+                          children: <TextSpan>[
+                            const TextSpan(
+                              text: "2. Masukkan nomor Virtual Account ",
+                            ),
+                            TextSpan(
+                              text: " 123 23456 0000 222\n",
+                              style: TextCollection().smallLabel.copyWith(
+                                    color: ColorApp().secondaryColor,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                            ),
+                            const TextSpan(text: "dan pilih Send\n"),
+                            const TextSpan(
+                              text:
+                                  "3. Periksa informasi yang tertera di layar. Pastikan Merchant adalah Digihouse, dan total tagihan sudah Benar.\n",
+                            ),
+                            const TextSpan(
+                              text: "4. Masukkan PIN m-BCA anda dan pilih OK",
+                            ),
+                          ],
                         ),
-                        const TextSpan(text: "dan pilih Send\n"),
-                        const TextSpan(
-                          text:
-                              "3. Periksa informasi yang tertera di layar. Pastikan Merchant adalah Digihouse, dan total tagihan sudah Benar.\n",
-                        ),
-                        const TextSpan(
-                          text: "4. Masukkan PIN m-BCA anda dan pilih OK",
-                        ),
-                      ],
+                      ),
                     ),
                   ),
-                ),
+                  const SizedBox(height: 10),
+                  Column(
+                    children: [
+                      Visibility(
+                        visible: _isVisible,
+                        child: Row(
+                          children: [
+                            Text(
+                              "Petunjuk Transfer iBanking",
+                              style: TextCollection().bodySmall.copyWith(
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                            ),
+                            const Spacer(),
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isShowiBanking = !_isShowiBanking;
+                                  // _isShowmBanking = !_isShowmBanking;
+                                });
+                              },
+                              icon: _isShowiBanking
+                                  ? const Icon(Icons.keyboard_arrow_down)
+                                  : const Icon(Icons.keyboard_arrow_up),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Stack(
+                        children: [
+                          const SizedBox(height: 50),
+                          Visibility(
+                            visible: _isVisible && !_isShowiBanking,
+                            maintainSize: true,
+                            maintainAnimation: true,
+                            maintainState: true,
+                            child: SizedBox(
+                              width: double.infinity,
+                              child: RichText(
+                                text: TextSpan(
+                                  text:
+                                      "1. Pilih i-Transfer > BCA Virtual Account\n",
+                                  style: TextCollection().bodySmall.copyWith(
+                                        fontWeight: FontWeight.normal,
+                                        fontSize: 12,
+                                        height: 2.0,
+                                      ),
+                                  children: <TextSpan>[
+                                    const TextSpan(
+                                      text:
+                                          "2. Masukkan nomor Virtual Account ",
+                                    ),
+                                    TextSpan(
+                                      text: " 123 23456 0000 222\n",
+                                      style: TextCollection()
+                                          .smallLabel
+                                          .copyWith(
+                                            color: ColorApp().secondaryColor,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                    ),
+                                    const TextSpan(text: "dan pilih Send\n"),
+                                    const TextSpan(
+                                      text:
+                                          "3. Periksa informasi yang tertera di layar. Pastikan Merchant adalah Digihouse, dan total tagihan sudah Benar.\n",
+                                    ),
+                                    const TextSpan(
+                                      text:
+                                          "4. Masukkan PIN i-BCA anda dan pilih OK",
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              Container(
-                height: 30,
-                width: double.infinity,
-                color: Colors.green,
-              )
             ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.all(16.0),
+        height: MediaQuery.of(context).size.height / 10.5,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: ColorApp().mainColor,
+        ),
+        child: SizedBox(
+          height: 40,
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: ColorApp().secondaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+            onPressed: () {},
+            child: Text(
+              "OK",
+              style: TextCollection().bodySmall.copyWith(
+                    color: ColorApp().light4,
+                  ),
+            ),
           ),
         ),
       ),
