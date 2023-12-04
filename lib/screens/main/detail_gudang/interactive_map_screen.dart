c42a7b74e8a6307139dbadf57529e347a9588352
itsMallix <amandamaulana00@gmail.com>
2023-12-04 21:49:43 +0700

    docs : starting interactive map

---
+++ lib/screens/main/detail_gudang/interactive_map_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InteractiveMap extends StatefulWidget {
+  const InteractiveMap({super.key});
+
+  @override
+  State<InteractiveMap> createState() => _InteractiveMapState();
+}
+
+class _InteractiveMapState extends State<InteractiveMap> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
